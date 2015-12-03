b84d7b326232a51e33762258a659147394d88cd0
Ken Schnall <kenneth.schnall@gmail.com>
2015-12-03 14:08:47 -0500

    Refactored project, added image to home view

---
+++ behrend-lion-ambassadors/app/controllers/main_controller.rb
@@ -0,0 +1,20 @@
+class MainController < ApplicationController
+	def home
+	end
+	def join_us
+	end
+	def events
+	end
+	def photos
+	end
+	def about
+	end
+	def contact
+	end
+	def facebook
+	end
+	def twitter
+	end
+	def instagram
+	end
+end
