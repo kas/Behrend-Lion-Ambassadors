4b6b93ba25192662b29afc75cd89cdbfa0351980
Ken Schnall <kenneth.schnall@gmail.com>
2016-01-06 15:59:33 -0500

    added image carousel to home page

--- behrend-lion-ambassadors/config/initializers/assets.rb
+++ behrend-lion-ambassadors/config/initializers/assets.rb
@@ -9,3 +9,15 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( home.js )
+Rails.application.config.assets.precompile += %w( carousel2.jpg )
+Rails.application.config.assets.precompile += %w( owl1.jpg )
+Rails.application.config.assets.precompile += %w( owl2.jpg )
+Rails.application.config.assets.precompile += %w( owl.carousel.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.css )
+Rails.application.config.assets.precompile += %w( owl.theme.css )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.js )
+Rails.application.config.assets.precompile += %w( carousel3.jpg )
+Rails.application.config.assets.precompile += %w( carousel4.jpg )
